9264dd2f7820ffa5fe0ab0aea4e6cd987debf6f6
cafkarembo <dorcaskavata0@gmail.com>
2024-09-03 14:22:41 +0300

    none
    files

---
+++ hello.py
@@ -0,0 +1,73 @@
+#print("hello world")
+"""
+room_917 = 5 #variable
+room_8 = 4
+
+room_9 = "five people"
+
+print(room_917 + room_8)
+
+room_10 = [2, 5,7,85,41,2] #Array
+
+print(room_10)
+
+print(room_10[0])
+"""
+
+"""x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)"""
+
+Dorcas = 64
+
+def myfunc():
+    print("Dorcas is", + Dorcas, "years old")
+
+myfunc()
+
+"""dorcas = 64
+
+print("Dorcas is", + dorcas, "years old")"""
+
+"""def func1():
+    if ( 2 > 3):
+        print(True)
+    else:
+        print(False)
+
+func1()
+
+def func1():
+    if ( 2 < 3):
+        print(True)
+    else:
+        print(False)
+
+func1()
+
+def func1():
+    if ( 2 != 3):
+        print(True)
+    else:
+        print(False)
+
+func1()"""
+
+"""def func1():
+    if ( 3 != 3 ):
+        print(True)
+    else:
+        print(False)
+
+func1()"""
+
+def func2():
+    if ( 22 < 3):
+        print(True)
+    elif( 22 >= 3):
+        print(True)
+    else:
+        print(False)
+
+func2()
---
+++ proj1.py
@@ -0,0 +1,11 @@
+print("i love ice cream")
+print("God bless my life")
+
+print(2 + 3, "i LOVE MATHS")
+print("2+3", "the answer is 5")
+
+print("The answer of: ", "2-3", "is", "-1")
+print("the answer of 2-3 is -1")
+print("2-3", "the answer is -1")
+
+print(20*30)
